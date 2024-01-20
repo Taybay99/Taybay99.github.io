68a1b7144f34b378810029ad0fe1d5e53e3244f1
Taybay99 <43314961+Taybay99@users.noreply.github.com>
2024-01-20 14:25:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
 let btn7 = document.getElementById("btn7");
-let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
 
 btn1.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
