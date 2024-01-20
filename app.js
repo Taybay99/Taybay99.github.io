3e1abee3c837513e0ea60455f159667532d603f9
Taybay99 <43314961+Taybay99@users.noreply.github.com>
2024-01-20 14:18:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
+let btn7 = document.getElementById("btn7");
+let btn7 = document.getElementById("btn7");
 
 btn1.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
@@ -80,6 +82,27 @@ btn6.addEventListener("click", function(){
 	}
 });
 
+btn7.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 7!");
+		item = "7";
+		tg.MainButton.show();
+	}
+});
+
+btn8.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 8!");
+		item = "8";
+		tg.MainButton.show();
+	}
+});
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
