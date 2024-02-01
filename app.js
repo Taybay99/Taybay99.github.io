ef59bbee52d6e9261fe35c15a0e90a618e56a8b7
Taybay99 <43314961+Taybay99@users.noreply.github.com>
2024-02-01 15:26:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,45 +7,107 @@ tg.MainButton.color = '#2cab37';
 
 let item = "";
 
-// Функция для отображения выбранной страницы
-function showPage(pageId) {
-    // Скрываем все страницы
-    document.querySelectorAll('.item').forEach(item => {
-        item.style.display = 'none';
-    });
-
-    // Отображаем выбранную страницу
-    document.getElementById(pageId).style.display = 'block';
-}
-
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
 
 btn1.addEventListener("click", function(){
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText("Вы выбрали урок 1!");
-        item = "1";
-        showPage("page2"); // Переключаемся на вторую страницу
-        tg.MainButton.show();
-    }
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали урок 1!");
+		item = "1";
+		tg.MainButton.show();
+	}
 });
 
 btn2.addEventListener("click", function(){
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText("Вы выбрали урок 2!");
-        item = "2";
-        showPage("page1"); // Переключаемся на первую страницу
-        tg.MainButton.show();
-    }
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 2!");
+		item = "2";
+		tg.MainButton.show();
+	}
+});
+
+btn3.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 3!");
+		item = "3";
+		tg.MainButton.show();
+	}
+});
+
+btn4.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 4!");
+		item = "4";
+		tg.MainButton.show();
+	}
+});
+
+btn5.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 5!");
+		item = "5";
+		tg.MainButton.show();
+	}
+});
+
+btn6.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 6!");
+		item = "6";
+		tg.MainButton.show();
+	}
+});
+
+btn7.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 7!");
+		item = "7";
+		tg.MainButton.show();
+	}
+});
+
+btn8.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 8!");
+		item = "8";
+		tg.MainButton.show();
+	}
 });
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
-    tg.sendData(item);
+	tg.sendData(item);
 });
+
 
 let usercard = document.getElementById("usercard");
 
