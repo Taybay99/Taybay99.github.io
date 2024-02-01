3ca11a2de4a99f76a25442c6a1791079cc0f70d5
Taybay99 <43314961+Taybay99@users.noreply.github.com>
2024-02-01 15:44:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 let tg = window.Telegram.WebApp;
-let currentPage = document.getElementById("page1");
-let nextPage = document.getElementById("page2");
+
+tg.expand();
+
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
 
 let item = "";
 
 let btn1 = document.getElementById("btn1");
-// Добавьте кнопки для остальных уроков
+let backBtn = document.getElementById("backBtn");
+let page1 = document.getElementById("page1");
+let page2 = document.getElementById("page2");
 
 btn1.addEventListener("click", function(){
-    // Ваши текущие действия
-
-    // Переключение страниц
-    currentPage.style.display = "none";
-    nextPage.style.display = "block";
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы выбрали урок 1!");
+        item = "1";
+        tg.MainButton.show();
+        // Переключение на страницу с содержанием урока 1
+        page1.style.display = "none";
+        page2.style.display = "block";
+    }
 });
 
-// Добавьте аналогичные обработчики для остальных кнопок
+// Добавьте обработчик для кнопки "назад"
+backBtn.addEventListener("click", function(){
+    // Переключение страниц обратно
+    page1.style.display = "block";
+    page2.style.display = "none";
+});
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
     tg.sendData(item);
 });
 
 let usercard = document.getElementById("usercard");
+
 let p = document.createElement("p");
 
 p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
