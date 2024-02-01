7a0a390094fea9860ce1ee03363288dc7ccb0d0d
Taybay99 <43314961+Taybay99@users.noreply.github.com>
2024-02-01 15:14:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,57 +1,43 @@
-// Объявите переменную для хранения текущего урока
-let currentLesson = "";
+let tg = window.Telegram.WebApp;
 
-// Функция для обновления контента при выборе урока
-function updateContent(lesson) {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        tg.MainButton.setText(`Вы выбрали урок ${lesson}!`);
-        currentLesson = lesson;
-        tg.MainButton.show();
-    }
+tg.expand();
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+
+let item = "";
+
+// Функция для отображения выбранной страницы
+function showPage(pageId) {
+    // Скрываем все страницы
+    document.querySelectorAll('.item').forEach(item => {
+        item.style.display = 'none';
+    });
+
+    // Отображаем выбранную страницу
+    document.getElementById(pageId).style.display = 'block';
 }
 
-// Добавьте обработчики событий для каждой кнопки
-btn1.addEventListener("click", function() {
-    updateContent("1");
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+// Добавьте аналогичные переменные для других кнопок
+
+btn1.addEventListener("click", function(){
+    showPage("page1");
+    item = "1";
 });
 
-btn2.addEventListener("click", function() {
-    updateContent("2");
+btn2.addEventListener("click", function(){
+    showPage("page2");
+    item = "2";
+});
+// Добавьте аналогичные обработчики для других кнопок
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+    tg.sendData(item);
 });
 
-btn3.addEventListener("click", function() {
-    updateContent("3");
-});
-
-btn4.addEventListener("click", function() {
-    updateContent("4");
-});
-
-btn5.addEventListener("click", function() {
-    updateContent("5");
-});
-
-btn6.addEventListener("click", function() {
-    updateContent("6");
-});
-
-btn7.addEventListener("click", function() {
-    updateContent("7");
-});
-
-btn8.addEventListener("click", function() {
-    updateContent("8");
-});
-
-// Добавьте обработчик события для кнопки "Главный"
-Telegram.WebApp.onEvent("mainButtonClicked", function() {
-    tg.sendData(currentLesson);
-});
-
-// Остальной ваш код оставьте без изменений
 let usercard = document.getElementById("usercard");
+
 let p = document.createElement("p");
 p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 usercard.appendChild(p);
